f5d1d83256f9989b5d82476912ea661614dc8253
Frank Verhoeven <frankverhoeven@me.com>
2018-02-14 20:39:32 +0100

    Update for elastic. Include logo images. Auto-set config skin_logo

--- fv_logo/fv_logo.php
+++ fv_logo/fv_logo.php
@@ -3,9 +3,9 @@
 /**
  * FV Logo
  *
- * Plugin to resize the logo
+ * Custom FV Logo
  *
- * @version 1.0
+ * @version 1.1
  * @author Frank Verhoeven <hi@frankverhoeven.me>
  */
 class fv_logo extends rcube_plugin
@@ -15,9 +15,26 @@ class fv_logo extends rcube_plugin
      */
     public function init()
     {
+        $this->add_hook('config_get', [$this, 'modifyConfig']);
+
         $skin_path = $this->local_skin_path();
         if (is_file($this->home . "/$skin_path/fvlogo.css")) {
             $this->include_stylesheet("$skin_path/fvlogo.css");
         }
+    }
+
+    /**
+     * Modify config to set logo
+     *
+     * @param array $config
+     * @return array
+     */
+    public function modifyConfig($config)
+    {
+        if (is_array($config) && isset($config['name']) && 'skin_logo' == $config['name'] && 'skins/larry' == $this->local_skin_path()) {
+            $config['result'] = $this->url($this->local_skin_path() . '/images/logo-white.svg');
+        }
+
+        return $config;
     }
 }
