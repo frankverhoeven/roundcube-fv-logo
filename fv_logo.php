957d35a38e7466221d0a60c9e86266786d9e1001
Frank Verhoeven <frankverhoeven@me.com>
2018-08-13 21:09:10 +0200

    Move in place

--- fv_logo.php
+++ fv_logo.php
@@ -1,24 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * FV Logo
- *
- * Custom FV Logo
- *
- * @version 1.1
  * @author Frank Verhoeven <hi@frankverhoeven.me>
  */
-class fv_logo extends rcube_plugin
+final class fv_logo extends rcube_plugin
 {
-    /**
-     * Initialization method, needs to be implemented by the plugin itself
-     */
     public function init()
     {
         $this->add_hook('config_get', [$this, 'modifyConfig']);
 
         $skin_path = $this->local_skin_path();
-        if (is_file($this->home . "/$skin_path/fvlogo.css")) {
+        if (\is_file($this->home . "/$skin_path/fvlogo.css")) {
             $this->include_stylesheet("$skin_path/fvlogo.css");
         }
     }
@@ -26,12 +20,16 @@ class fv_logo extends rcube_plugin
     /**
      * Modify config to set logo
      *
-     * @param array $config
-     * @return array
+     * @param mixed $config
+     * @return mixed
      */
     public function modifyConfig($config)
     {
-        if (is_array($config) && isset($config['name']) && 'skin_logo' == $config['name'] && 'skins/larry' == $this->local_skin_path()) {
+        if (\is_array($config)
+            && isset($config['name'])
+            && 'skin_logo' === $config['name']
+            && 'skins/larry' === $this->local_skin_path()
+        ) {
             $config['result'] = $this->url($this->local_skin_path() . '/images/logo-white.svg');
         }
 
